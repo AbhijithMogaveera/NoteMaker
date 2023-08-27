e9faaebb79feb12ae40cdc86030b8094c6d518a5
Abhijth Mogaveera <abhialur8898@gmail.com>
2023-08-27 18:12:14 +0530

    UI fixes

--- app/src/main/java/com/abhijith/note_taking/MainActivity.kt
+++ app/src/main/java/com/abhijith/note_taking/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.abhijith.note_taking
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.card.MaterialCardView
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -21,5 +25,19 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
             )
         }
+        val cardView = findViewById<MaterialCardView>(R.id.materialCardView)
+        val shapeDrawable = GradientDrawable()
+        shapeDrawable.shape = GradientDrawable.RECTANGLE
+        shapeDrawable.cornerRadii = floatArrayOf(
+            resources.getDimension(R.dimen.top_left_radius),
+            resources.getDimension(R.dimen.top_left_radius),
+            resources.getDimension(R.dimen.top_right_radius),
+            resources.getDimension(R.dimen.top_right_radius),
+            resources.getDimension(R.dimen.bottom_right_radius),
+            resources.getDimension(R.dimen.bottom_right_radius),
+            resources.getDimension(R.dimen.bottom_left_radius),
+            resources.getDimension(R.dimen.bottom_left_radius)
+        )
+        cardView.background = shapeDrawable
     }
 }
--- notes/src/main/java/com/abhijith/notes/screens/creation/NoteUpsertFragment.kt
+++ notes/src/main/java/com/abhijith/notes/screens/creation/NoteUpsertFragment.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -128,7 +129,7 @@ class NoteUpsertFragment : BindingFragment<NoteCreationBinding>() {
             findNavController().navigateUp()
         }) {
             Icon(
-                imageVector = Icons.Default.ArrowBack,
+                imageVector = Icons.Default.Close,
                 contentDescription = null,
                 tint = color
             )
